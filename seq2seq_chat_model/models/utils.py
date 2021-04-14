90ff06824527f08d732788b02e6853330490e4aa
Robin Schmitt <robin.p.schmitt@freenet.de>
2021-04-14 11:15:11 +0200

    Reworked the beam search function. Added encoding and decoding functions for both RNNs and Transformers

--- seq2seq_chat_model/models/utils.py
+++ seq2seq_chat_model/models/utils.py
@@ -80,103 +80,96 @@ def get_decoder_input(answer_group: List[List[str]], dataset):
 
     return torch.tensor(answer)
 
+def get_encoding_rnn(input_tensor, encoder):
+    batch_size = input_tensor.shape[0]
+    hidden = cell = encoder.init_hidden(batch_size)
 
-def decode_beam(inp, encoder, decoder, dataset, beam_width):
+    out, _, _ = encoder(input_tensor, hidden, cell)
+
+    return out
+
+def get_encoding_trans(input_tensor, encoder):
+    return encoder(input_tensor)
+
+def get_decoding_rnn(target_tensor, enc_outputs, decoder):
+    batch_size = target_tensor.shape[0]
+    dec_hidden = dec_cell = decoder.init_hidden(batch_size)
+
+    dec_outputs = []
+
+    for i in range(target_tensor.shape[1]):
+        dec_input = target_tensor[:, i].view(-1, 1)
+
+        dec_output, dec_hidden, dec_cell = decoder(
+            dec_input, dec_hidden, dec_cell, enc_outputs
+        )
+
+        dec_outputs.append(dec_output)
+
+    return torch.cat(dec_outputs, dim = 1)
+
+def get_decoding_trans(target_tensor, enc_outputs, decoder):
+    return decoder(target_tensor, enc_outputs)
+
+def decode_beam(input_tensor, encoder, decoder, encoding_func, decoding_func, beam_width, sos_index, num_steps):
     """Return the beam_width top predictions of the decoder given an input."""
-    # go into eval mode (disable dropout)
-    encoder.eval()
-    decoder.eval()
-
-    hidden_size = encoder.hidden_size
-    num_enc_lay = encoder.num_layers
-    batch_size = 1
-
-    top_picks = []
+    batch_size = input_tensor.shape[0]
 
     with torch.no_grad():
-        # init encoder inputs
-        enc_hidden = encoder.init_hidden(batch_size)
-        enc_cell = encoder.init_hidden(batch_size)
-        enc_in = inp.view(batch_size, -1).to(device)
-
-        # obtain encoder outputs
-        enc_outputs, enc_hidden, enc_cell = encoder(
-            enc_in, enc_hidden, enc_cell
-        )
-
-        # prepare hidden encoder state for decoder
-        enc_hidden = enc_hidden.view(num_enc_lay, 2, batch_size, hidden_size)
-        enc_hidden = torch.cat(
-            (enc_hidden[-1, 0], enc_hidden[-1, 1]), dim=1
-        ).view(1, batch_size, hidden_size * 2)
-
-        # init decoder inputs
-        dec_hidden = decoder.init_hidden(batch_size)
-        dec_cell = decoder.init_hidden(batch_size)
-        dec_in = torch.tensor(dataset.vocab["<start>"]).view(1, 1).to(device)
-
-        # obtain first decoder outputs
-        dec_out, dec_hidden, dec_cell = decoder(
-            dec_in, enc_outputs, enc_hidden, dec_hidden, dec_cell
-        )
+        # get encoder sequence
+        enc_outputs = encoding_func(input_tensor, encoder)
+        # first decoder input is the sos token (seq len = 1)
+        dec_in = torch.tensor([sos_index] * batch_size).view(batch_size, 1).to(device)
+        # get the decoder predictions
+        dec_out = decoding_func(dec_in, enc_outputs, decoder)
 
         # get first top predictions
-        top_k = torch.topk(F.softmax(dec_out, 1), beam_width, 1)
+        top_k = torch.topk(F.softmax(dec_out, dim = -1), k = beam_width, dim = -1)
 
-        # save parameters of the first top picks
-        top_picks = [
-            {
-                "seq": [token],
-                "prob": np.log(prob.item()),
-                "hid": dec_hidden,
-                "cell": dec_cell,
-            }
-            for prob, token in zip(top_k[0][0], top_k[1][0])
-        ]
+        # top len 1 sequences of shape (batch, beam_width, 1)
+        top_seqs = top_k[1].transpose(1, 2)
+        # corresponding log probs of shape (batch, beam_width)
+        top_log_probs =  torch.log(top_k[0]).squeeze()
+        print(top_seqs.shape)
+        print(top_log_probs.shape)
+                 
 
-        # do 10 decoding steps
-        for i in range(10):
-            hypotheses = []
+        # do num_steps decoding steps
+        for i in range(num_steps):
 
-            # go through every current top pick
-            for pick in top_picks:
-                # the lastly predicted symbol is the next input
-                dec_in = pick["seq"][-1].view(1, 1)
+            # merge batch and beam width dimensions
+            # shape (batch * beam_width, seq_len)
+            dec_in = top_seqs.view(batch_size * beam_width, -1)
+            # pass the current top sequences to the decoder
+            dec_out = decoding_func(dec_in, enc_outputs, decoder)
+ 
+            # for every batch and beam, get the next top k predictions
+            probs, tokens = torch.topk(F.softmax(dec_out[:, -1], dim = -1), k = beam_width, dim = -1)
+            # separate batch and beam dimension and merge the two beam dimensions instead
+            # (for every previous beam, there are now k new beams)
+            tokens = tokens.view(batch_size, beam_width * beam_width, 1)
+            # for every previous beam, repeat the corresponding sequence for beam_width times
+            seqs = top_seqs.repeat_interleave(repeats = beam_width, dim = 1)
+            # append the new beam predictions to the corresponding previous beam sequence
+            seqs = torch.cat([seqs, tokens], dim = -1)
 
-                # get the next outputs
-                dec_out, dec_hidden, dec_cell = decoder(
-                    dec_in, enc_outputs, enc_hidden, pick["hid"], pick["cell"]
-                )
+            # separate batch and previous beam dimension
+            # (for every previous beam prob, there are now k new beam probs)
+            probs = probs.view(batch_size, beam_width, beam_width)
+            # add the log probs of the previous beams to the corresponding log probs
+            # of the new beams (corresponds to the products of the non-log probs)
+            log_probs = top_log_probs[:, :, None] + torch.log(probs)
+            # merge the beam dimensions
+            log_probs = log_probs.view(batch_size, beam_width * beam_width)
+            # get the new k top beams
+            top_log_probs, top_indices = torch.topk(log_probs, k = beam_width, dim = -1)
+            
+            # select the new top sequences as the top indices of the probabilities
+            top_seqs = seqs[torch.arange(seqs.shape[0]), top_indices],
 
-                # get next top picks of the current hypothesis
-                top_k = torch.topk(F.softmax(dec_out, 1), beam_width, 1)
+    top_probs = torch.exp(top_log_probs)
 
-                # store parameters of the top picks
-                picks = [
-                    {
-                        "seq": pick["seq"] + [token],
-                        "prob": np.log(prob.item()) + pick["prob"],
-                        "hid": dec_hidden,
-                        "cell": dec_cell,
-                    }
-                    for prob, token in zip(top_k[0][0], top_k[1][0])
-                ]
-
-                # add to current hypothesis
-                hypotheses += picks
-
-            # sort after probability
-            hypotheses = sorted(
-                hypotheses, key=operator.itemgetter("prob"), reverse=True
-            )
-
-            # get top k hyptheses
-            top_picks = hypotheses[:beam_width]
-
-    top_probs = [np.exp(pick["prob"].item()) for pick in top_picks]
-    top_seqs = [pick["seq"] for pick in top_picks]
-
-    return list(zip(top_probs, top_seqs))
+    return top_seqs, top_probs
 
 
 def clean_dec_output(sequence):
